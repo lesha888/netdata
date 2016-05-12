3fa58168de7fdc8abe3a86121fd79ecc7d21992d
lesha888 <lesha888@users.noreply.github.com>
2016-05-13 01:27:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ DEBIAN_FRONTEND=noninteractive
 
 # install dependencies for build
 
-apt-get -qq -y install zlib1g-dev gcc make git autoconf autogen automake pkg-config netcat-openbsd jq
+apt-get -qq -y install zlib1g-dev gcc make git autoconf autogen automake pkg-config netcat-openbsd jq mysql-client
+
 
 # fetch netdata
 
@@ -31,3 +32,8 @@ rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 ln -sf /dev/stdout /var/log/netdata/access.log
 ln -sf /dev/stdout /var/log/netdata/debug.log
 ln -sf /dev/stderr /var/log/netdata/error.log
+
+cat <<DATA
+mysql_cmds[local]="/usr/bin/mysql"
+mysql_opts[local]="-h ${MYSQL_LOCAL_HOST} -u ${MYSQL_LOCAL_USER} -p ${MYSQL_LOCAL_PASSWORD} --port=${MYSQL_LOCAL_PORT} "
+DATA > /etc/netdata/mysql.conf 
